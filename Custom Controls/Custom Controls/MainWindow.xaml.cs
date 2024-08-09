eea8de41f5a5b9eff3d0f63847d35984f45d79bb
Tirtharaj Das <tirtharaj.das@entecnia.com>
2024-08-09 17:21:01 +0530

    Add files via upload

---
+++ Custom Controls/Custom Controls/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Custom_Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Custom Controls/Custom Controls/View/customControls/TxtBoxes.xaml.cs
@@ -0,0 +1,52 @@
+﻿using OxyPlot.Series;
+using OxyPlot;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Custom_Controls.View.customControls
+{
+    /// <summary>
+    /// Interaction logic for TxtBoxes.xaml
+    /// </summary>
+    public partial class TxtBoxes : UserControl
+    {
+        public TxtBoxes()
+        {
+            InitializeComponent();
+            var model = new PlotModel { Title = "Example Chart" };
+            var series = new LineSeries
+            {
+                Title = "Line Series",
+                MarkerType = MarkerType.Circle
+            };
+
+            series.Points.Add(new DataPoint(0, 0));
+            series.Points.Add(new DataPoint(10, 18));
+            series.Points.Add(new DataPoint(20, 12));
+            series.Points.Add(new DataPoint(30, 8));
+            series.Points.Add(new DataPoint(40, 15));
+
+            model.Series.Add(series);
+
+            DataContext = new { MyModel = model };
+        }
+
+        //public void btn_Clear(object sender, RoutedEventArgs s)
+        //{
+        //    txt_Box.Clear();
+        //    txt_Box.Focus();
+        //}
+        //public void VisiblityChecker(object sender, RoutedEventArgs s)
+        //{
+        //    if (string.IsNullOrEmpty(txt_Box.Text))
+        //    {
+        //        txt_placeholder.Visibility = Visibility.Visible;
+
+        //    }
+        //    else
+        //    {
+        //        txt_placeholder.Visibility = Visibility.Hidden;
+        //    }
+        //}
+    }
+}
